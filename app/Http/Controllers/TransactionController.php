b04ba742f127f77ab75471bb32224c0180f8028e
Ahmed Adel <ahmedadelelghiati@gmail.com>
2026-07-04 17:14:51 +0300

    feat: implement show, update, and destroy protected endpoints for transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -18,10 +18,25 @@ class TransactionController extends Controller
         ] , 200);
     }
 
+    public function show(Transaction $transaction)
+    {
+        if(Auth::id() != $transaction->user_id) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have permission to view this transaction',
+            ] , 403);
+        }
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction details',
+            'data' => $transaction
+        ] , 200);
+    }
+
     public function store(Request $request) {
         $validatedData = $request->validate([
             'title' => ['required' , 'string' , 'min:3' , 'max:255'],
-            'description' => ['required' , 'string' , 'nullable' , 'max:2000'],
+            'description' => ['string' , 'nullable' , 'max:2000'],
             'amount' => ['required' , 'numeric' , 'min:0.01'],
             'type' => ['required' , 'in:income,expense'],
             'date' => ['required' , 'date']
@@ -34,5 +49,42 @@ class TransactionController extends Controller
             'message' => 'Transaction created',
             'data' => $validatedData
         ] , 201);
+    }
+    public function update(Request $request , Transaction $transaction)
+    {
+        if(Auth::id() != $transaction->user_id) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have permission to update this transaction',
+            ] , 403);
+        }
+
+        $validatedData = $request->validate([
+            'title' => ['required' , 'string' , 'min:3' , 'max:255'],
+            'description' => ['string' , 'nullable' , 'max:2000'],
+            'amount' => ['required' , 'numeric' , 'min:0.01'],
+            'type' => ['required' , 'in:income,expense'],
+            'date' => ['required' , 'date']
+        ]);
+
+       $transaction->update($validatedData);
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction updated',
+            'data' => $transaction
+        ] , 200);
+    }
+
+    public function destroy(Transaction $transaction) {
+        if(Auth::id() != $transaction->user_id) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have permission to delete this transaction',
+            ] , 403);
+        }
+        $transaction->delete();
+        return response()->json([] , 204);
     }
 }
